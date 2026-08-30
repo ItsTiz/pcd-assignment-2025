d6b872ba3dde7a26e655905ed421a97e808220ff
inesfraccalvierii <ines.fraccalvieri@studio.unibo.it>
2026-08-30 15:23:23 +0200

    refactor: throttle mouse-driven Move commands in LocalView

--- distributed-akka/src/main/scala/it/unibo/agar/distributed/view/LocalView.scala
+++ distributed-akka/src/main/scala/it/unibo/agar/distributed/view/LocalView.scala
@@ -1,15 +1,19 @@
 package it.unibo.agar.distributed.view
 
 import it.unibo.agar.distributed.model.GameStateManager
-
 import java.awt.Graphics2D
-import scala.swing.event.MouseMoved
-import scala.swing.{Dimension, MainFrame, Panel}
+import javax.swing.Timer
+import scala.swing.*
 
 class LocalView(manager: GameStateManager, playerId: String) extends MainFrame:
 
   title = s"Agar.io - Local View ($playerId)"
   preferredSize = new Dimension(400, 400)
+
+  private val MoveIntervalMillis = 50
+  
+  @volatile private var desiredDx: Double = 0.0
+  @volatile private var desiredDy: Double = 0.0
 
   contents = new Panel:
     listenTo(keys, mouse.moves)
@@ -18,18 +22,22 @@ class LocalView(manager: GameStateManager, playerId: String) extends MainFrame:
 
     override def paintComponent(g: Graphics2D): Unit =
       val world = manager.getWorld
-      val playerOpt = world.playerById(playerId)
+      val playerOpt = world.players.find(_.id == playerId)
       val (offsetX, offsetY) = playerOpt
         .map(p => (p.x - size.width / 2.0, p.y - size.height / 2.0))
         .getOrElse((0.0, 0.0))
       AgarViewUtils.drawWorld(g, world, offsetX, offsetY, Some(playerId))
 
-    reactions += { case e: MouseMoved =>
+    reactions += { case e: event.MouseMoved =>
+    
       val mousePos = e.point
-      val playerOpt = manager.getWorld.playerById(playerId)
-      playerOpt.foreach: player =>
-        val dx = (mousePos.x - size.width / 2) * 0.01
-        val dy = (mousePos.y - size.height / 2) * 0.01
-        manager.movePlayerDirection(playerId, dx, dy)
-      repaint()
+      desiredDx = (mousePos.x - size.width / 2) * 0.01
+      desiredDy = (mousePos.y - size.height / 2) * 0.01
     }
+
+  // invia il comando Move a intervalli fissi, indipendentemente dalla frequenza del mouse
+  private val moveTimer = new Timer(MoveIntervalMillis, _ => {
+    if desiredDx != 0.0 || desiredDy != 0.0 then
+      manager.movePlayerDirection(playerId, desiredDx, desiredDy)
+  })
+  moveTimer.start()
